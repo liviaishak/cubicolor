4ed365668536355490f5f15b5f2e5dd11564f762
Livia <liviaishak1@gmail.com>
2018-06-25 22:10:55 -0700

    fixed the time

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
-/*
- * Each object of the square holds the time it was
- * changed in color and the ID to reference in index.html
- */
+// each object of the square holds the time it was
+// changed in color and the ID to reference in index.html
+
 class Square {
-  /*
-   * Initiate all of the variable of class square
-   * And change the color
-   */
+// initiate all of the variable of class square
+// and change the color
+
   constructor(identification) {
-    this.color = '#FFFFFF'; // default to white
+    this.color = '#FFFFFF'; // set default to white
     this.id = identification;
     this.time = 0;
   }
 
-  /*
-   * Check if the square has changed in color in the past 2 seconds
-   */
+// check if the square has changed in color in the past 2000 ms
   ifMoreThanTwoSeconds() {
 
     var currentTime = Date.now();
-    if((currentTime - this.time) >= 2) {
+
+    // if the square's color is white (starting point)
+    if(this.time == 0) {
+      this.time = Date.now();
       return true;
-      this.time = currentTime;
     }
 
+    if((currentTime - this.time) > 2000) {
+      // print out the difference in time (ms) between the second color change
+      // to the first color change
+      console.log(currentTime - this.time);
+      this.time = currentTime;
+      return true;
+    }
     return false;
   }
 
-  /*
-   * Set Random Colors
-   */
+  // set random colors
   setColor() {
 
-      // This is the possible combinations of colors to choose from
+      // possible combinations of colors to choose from
       var x = '0123456789ABCDEF';
-      // This is the first char should be
       var y = '#';
 
       for(var i = 0; i < 6; i++) {
-         // Generate a random color here
          y = y + x[Math.round(Math.random()*15)];
       }
 
-      // Initialize the time to current time if time == 0
+      // initialize the time to current time if time == 0
       if(this.time == 0) {
         this.time = Date.now();
         document.getElementById(this.id).style.backgroundColor = y;
         return;
       }
 
-      // Change color starts here
+      // change color starts here
       if(this.ifMoreThanTwoSeconds()) {
         document.getElementById(this.id).style.backgroundColor = y;
       }
   }
 }
 
-/*
- * This main will initiate all of the corresponding squares
- * with an object of Class Square
- */
+// this main will initiate all of the corresponding squares
+// with an object of Class Square
  var square1 = new Square("1");
  var square2 = new Square("2");
  var square3 = new Square("3");
@@ -77,19 +76,17 @@ class Square {
  var square15 = new Square("15");
  var square16 = new Square("16");
 
- // Store the squares in an array to get index
+ // store the squares in an array to get index
  var squares = [square1, square2, square3, square4, square5, square6,
                       square7, square8, square9, square10, square11, square12,
                       square13, square14, square15, square16 ];
 
- // Generate a random number to select a random square
+ // generate a random number to select a random square
 function selectOneSquareAndChangeColor() {
   var randomNum = Math.floor(1 + Math.random() * 16);
   var referenceSquare = squares[randomNum-1];
   referenceSquare.setColor();
 }
 
-/*
- * Main calling functions here
- */
+// main calling functions
 var test = setInterval(function(){selectOneSquareAndChangeColor()}, 250);
